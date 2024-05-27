6e86514ad2e66e03782458a2cb09c681f69727f7
Temi <tolaoluniyi@gmail.com>
2024-05-27 15:51:55 -0400

    Init

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 #------------------------------------------------------------------------------------------#
+# Class Project
+# Requirements
+# 1. Use terraform as Iaac
+# How will we accomplish this task with Terraform?
+# 1. Create the VPC
+# 2. Create the Internet Gateway and attach it to the VPC using a Route Table
+# 3. Create a Public Subnet and associate it with the Route Table
+# 4. Create a Security Group for the EC2 Instance
+# 5. Create the EC2 Instance and attach a public IP and Key Pair to it.
+#-------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
 # Add Amazon Keys
-#------------------------------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
 {
     
 }
